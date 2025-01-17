be0558f4d3e27f212ccf7463e562c7d053cc7d5e
Danu Pramei <helmy.prameireza@phincon.com>
2025-01-17 15:25:37 +0700

    Create EmployeeApp

--- shared/src/main/java/com/pcs/shared/Common.kt
+++ shared/src/main/java/com/pcs/shared/Common.kt
@@ -4,5 +4,3 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 
 typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
-
-typealias Inflate2<T> = (LayoutInflater) -> T
